f876adffe71cb56312ae991ad16cf69b44926922
Abraham Molleda <aamol.riv@gmail.com>
2023-02-07 01:05:03 -0600

    Mapeando Dtos

---
+++ WebAPI/Dtos/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Dtos
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+
+            //CreateMap<Producto, ProductoDto>();
+
+            CreateMap<Producto, ProductoDto>()
+                .ForMember(p => p.MarcaNombre, x => x.MapFrom(c => c.Marca.Nombre))
+                .ForMember(p => p.CategoriaNombre, x => x.MapFrom(c => c.Categoria.Nombre));
+        }
+    }
+}
